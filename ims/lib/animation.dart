aa2b9b7d8dff83fa512a7d59767306257744fe88
chedo <chetan2469@gmail.com>
2019-11-19 14:35:20 +0530

    first setup

---
+++ ims/lib/animation.dart
@@ -0,0 +1,19 @@
+import 'dart:math';
+import 'package:flutter/material.dart';
+
+class AnimatedLogo extends AnimatedWidget {
+  AnimatedLogo({Key key, Animation<double> animation})
+      : super(key: key, listenable: animation);
+
+  Widget build(BuildContext context) {
+    final animation = listenable as Animation<double>;
+    return Center(
+      child: Container(
+        margin: EdgeInsets.symmetric(vertical: 10),
+        height: animation.value,
+        width: animation.value,
+        child: FlutterLogo(),
+      ),
+    );
+  }
+}
